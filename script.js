95ae9f7a29c5d97f624b8dfc293fa41e43d4d763
patrickfrancca <patrick.francca@gmail.com>
2024-10-17 02:40:02 -0300

    Mobile responsive

--- script.js
+++ script.js
@@ -86,6 +86,8 @@ window.addEventListener('scroll', () => {
     }
 });
 
+
+
 //Arrumar deslocamento incorreto quando clica no botão preços da página "Sobre"
 document.querySelector('.b2 button').addEventListener('click', function(event) {
     event.preventDefault();
@@ -94,4 +96,49 @@ document.querySelector('.b2 button').addEventListener('click', function(event) {
         top: target.offsetTop - 100, // Ajuste o valor 100 para o deslocamento desejado
         behavior: 'smooth'
     });
+});
+
+// Adiciona um event listener ao evento de scroll
+window.addEventListener("scroll", function() {
+    var mouseContainer = document.querySelector('.container_mouse');
+    
+    // Verifica se a largura da tela é de um dispositivo móvel
+    if (window.innerWidth <= 768) {
+      // Aplica o fade-out ao rolar mais de 50px
+      if (window.scrollY > 50) {
+        mouseContainer.classList.add('hidden');
+      } else {
+        mouseContainer.classList.remove('hidden');
+      }
+    }
+  });
+  
+// Abrir o menu e mostrar o overlay
+document.getElementById("btn-menu").onclick = function() {
+    document.getElementById("menu-mobile").classList.add("abrir-menu");
+    document.querySelector(".overlay-menu").style.display = "block"; // Mostra o overlay
+    document.body.style.overflow = "hidden"; // Impede o scroll do fundo
+};
+
+// Fechar o menu e esconder o overlay
+document.querySelector(".btn-fechar").onclick = function() {
+    document.getElementById("menu-mobile").classList.remove("abrir-menu");
+    document.querySelector(".overlay-menu").style.display = "none"; // Esconde o overlay
+    document.body.style.overflow = "auto"; // Permite o scroll novamente
+};
+
+// Fecha o menu ao clicar no overlay
+document.querySelector(".overlay-menu").onclick = function() {
+    document.getElementById("menu-mobile").classList.remove("abrir-menu");
+    this.style.display = "none"; // Esconde o overlay
+    document.body.style.overflow = "auto"; // Permite o scroll novamente
+};
+
+// Fechar o menu ao clicar em um item do menu
+document.querySelectorAll('.menu-mobile nav ul li a').forEach(link => {
+    link.onclick = function() {
+        document.getElementById("menu-mobile").classList.remove("abrir-menu");
+        document.querySelector(".overlay-menu").style.display = "none"; // Esconde o overlay
+        document.body.style.overflow = "auto"; // Permite o scroll novamente
+    };
 });
