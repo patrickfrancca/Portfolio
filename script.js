ef20e40dfcac91a4131993589cf90ebdecebd1b6
patrickfrancca <patrick.francca@gmail.com>
2024-10-25 03:12:02 -0300

    Night mode implementation

--- script.js
+++ script.js
@@ -36,13 +36,16 @@ window.addEventListener("scroll", function() {
     }
 });
 
-// Função para rolar suavemente até uma seção específica
+// Função para rolar suavemente até o meio exato de uma seção
 function scrollToSection(index) {
     const section = sections[index];
     const headerOffset = 30; // Ajuste para o tamanho do cabeçalho fixo
-    const elementPosition = section.getBoundingClientRect().top;
-    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
 
+    // Calcula a posição central da seção e da janela
+    const sectionCenterPosition = section.getBoundingClientRect().top + window.pageYOffset + (section.getBoundingClientRect().height / 2);
+    const offsetPosition = sectionCenterPosition - (window.innerHeight / 2) - headerOffset;
+
+    // Executa a rolagem para a posição calculada
     window.scrollTo({
         top: offsetPosition,
         behavior: "smooth"
@@ -54,4 +57,39 @@ sections.forEach((section, index) => {
     section.addEventListener('click', () => {
         scrollToSection(index);
     });
+});
+
+
+
+// Obtém o botão de toggle
+const toggleSwitch = document.getElementById('dark-mode-toggle');
+
+// Função para habilitar o dark mode
+function enableDarkMode() {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('dark-mode', 'enabled');
+    toggleSwitch.checked = true;
+}
+
+// Função para desabilitar o dark mode
+function disableDarkMode() {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('dark-mode', 'disabled');
+    toggleSwitch.checked = false;
+}
+
+// Verifica a preferência salva ao carregar a página
+if (localStorage.getItem('dark-mode') === 'enabled') {
+    enableDarkMode();
+} else {
+    disableDarkMode();
+}
+
+// Alterna o dark mode e salva a preferência
+toggleSwitch.addEventListener('change', () => {
+    if (toggleSwitch.checked) {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 });
