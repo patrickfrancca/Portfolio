e2a81a58b32bf6d129beb0c30ac72e79d6937eba
patrickfrancca <patrick.francca@gmail.com>
2024-10-29 02:46:17 -0300

    Dark mode on mobile finish

--- script.js
+++ script.js
@@ -1,67 +1,4 @@
-const sections = document.querySelectorAll("section");
-const dots = document.querySelectorAll(".dot");
-let currentSectionIndex = 0;
-
-// Função para atualizar o indicador
-function updateIndicator(index) {
-    dots.forEach(dot => dot.classList.remove("active"));
-    dots[index].classList.add("active");
-}
-
-// Atualiza o indicador durante a rolagem
-window.addEventListener("scroll", () => {
-    let scrollPosition = window.scrollY + window.innerHeight / 2;
-
-    sections.forEach((section, index) => {
-        if (section.offsetTop <= scrollPosition && section.offsetTop + section.offsetHeight > scrollPosition) {
-            // Atualiza o indicador
-            currentSectionIndex = index; // Atualiza o índice atual
-            updateIndicator(currentSectionIndex);
-        }
-    });
-});
-
-// Atualiza o indicador na inicialização
-updateIndicator(currentSectionIndex);
-
-// Oculta o botão de scroll
-window.addEventListener("scroll", function() {
-    const scrollButton = document.querySelector(".btn");
-    
-    // Se o usuário rolar mais de 300px, o botão desaparece
-    if (window.scrollY > 300) {
-        scrollButton.classList.add("hidden");
-    } else {
-        scrollButton.classList.remove("hidden");
-    }
-});
-
-// Função para rolar suavemente até o meio exato de uma seção
-function scrollToSection(index) {
-    const section = sections[index];
-    const headerOffset = 30; // Ajuste para o tamanho do cabeçalho fixo
-
-    // Calcula a posição central da seção e da janela
-    const sectionCenterPosition = section.getBoundingClientRect().top + window.pageYOffset + (section.getBoundingClientRect().height / 2);
-    const offsetPosition = sectionCenterPosition - (window.innerHeight / 2) - headerOffset;
-
-    // Executa a rolagem para a posição calculada
-    window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth"
-    });
-}
-
-// Adiciona evento de rolagem suave ao clicar nas seções
-sections.forEach((section, index) => {
-    section.addEventListener('click', () => {
-        scrollToSection(index);
-    });
-});
-
-
-
-// Obtém o botão de toggle
+// Obtém o botão de toggle do dark mode
 const toggleSwitch = document.getElementById('dark-mode-toggle');
 
 // Função para habilitar o dark mode
@@ -94,6 +31,18 @@ toggleSwitch.addEventListener('change', () => {
     }
 });
 
+// Oculta o botão de scroll
+window.addEventListener("scroll", function() {
+    const scrollButton = document.querySelector(".btn");
+    
+    // Se o usuário rolar mais de 300px, o botão desaparece
+    if (window.scrollY > 300) {
+        scrollButton.classList.add("hidden");
+    } else {
+        scrollButton.classList.remove("hidden");
+    }
+});
+
 // Menu mobile
 
 // Seleciona o checkbox do botão do menu e o menu mobile
@@ -112,12 +61,23 @@ const menuLinks = document.querySelectorAll("#menu-mobile nav a");
 
 // Adiciona um evento de clique em cada link
 menuLinks.forEach(link => {
-  link.addEventListener("click", () => {
-    // Desmarca o checkbox para fechar o menu
-    btnMenuMob.checked = false;
-    
-    // Remove as classes 'abrir' e 'no-scroll' do body
-    menuMobile.classList.remove('abrir');
-    body.classList.remove('no-scroll');
-  });
+    link.addEventListener("click", () => {
+        // Desmarca o checkbox para fechar o menu
+        btnMenuMob.checked = false;
+        
+        // Remove as classes 'abrir' e 'no-scroll' do body
+        menuMobile.classList.remove('abrir');
+        body.classList.remove('no-scroll');
+    });
+});
+
+// Adiciona evento de clique ao toggle dentro do menu mobile
+const toggleInMenu = menuMobile.querySelector('#dark-mode-toggle');
+
+toggleInMenu.addEventListener('change', () => {
+    if (toggleInMenu.checked) {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 });
